02903d7064536360b9f6d5cb70e67f4ddf6090dd
teq-quocbang <bangthong@teqnological.asia>
2023-11-04 15:45:24 +0700

    feat: get with data in path

--- paths/path.go
+++ paths/path.go
@@ -35,5 +35,73 @@ func SetUpPaths(t *openapi2.T) {
 				},
 			},
 		},
+		"/user/info/{id}": {
+			Get: &openapi2.Operation{
+				Summary:     "get user info",
+				Description: "get user",
+				OperationID: "ListUserInfo",
+				Tags: []string{
+					"Account",
+				},
+				Parameters: openapi2.Parameters{
+					{
+						In:       "path",
+						Name:     "id",
+						Required: true,
+						Type:     "string",
+					},
+					{
+						In:       "body",
+						Name:     "body",
+						Required: true,
+						Schema: &openapi3.SchemaRef{
+							Value: &openapi3.Schema{
+								Properties: openapi3.Schemas{
+									"Data": &openapi3.SchemaRef{
+										Value: &openapi3.Schema{
+											Type: "object",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				Responses: map[string]*openapi2.Response{
+					"200": {
+						Description: "OK",
+						Schema: &openapi3.SchemaRef{
+							Value: &openapi3.Schema{
+								Properties: openapi3.Schemas{
+									"Data": &openapi3.SchemaRef{
+										Value: &openapi3.Schema{
+											Type: "array",
+											Items: &openapi3.SchemaRef{
+												Value: &openapi3.Schema{
+													Properties: openapi3.Schemas{
+														"Username": &openapi3.SchemaRef{
+															Value: &openapi3.Schema{
+																Type:        "string",
+																Description: "name of the user",
+															},
+														},
+														"Age": &openapi3.SchemaRef{
+															Value: &openapi3.Schema{
+																Type:        "integer",
+																Description: "user age",
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 }
